372cdcef206857e9f0e8418f92ab12e9084369ff
natdm02 <natalidimarino2002@gmail.com>
2024-04-16 16:29:10 +0200

    [add] password generation

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+
+$alphabet = [
+    'minusc' => 'abcdefghijklmnopqrstuvwxyz',
+    'maiusc' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    'numbers' => '1234567890',
+    'symbols' => '!"#$%&(){|}*+,-./:;<=>?@[\]^_~'
+];
+
+function generatePassword($arg, $allCharacters){
+    $password = [];
+    while (count($password) < $arg) {
+        foreach($allCharacters as $element){
+            $randNum = rand(0, strlen($element));
+            $password[] = $element[$randNum];
+        }
+    }
+    return implode($password);
+};
+
+$passlength = $_GET['pass-length'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +30,22 @@
     <title>php-strong-password-generator</title>
 </head>
 <body>
-    <h1>dcbwiqc</h1>
+    <main>
+    <h1>Strong Password Generator</h1>
+
+<form action="" method="get">
+    <label for="pass-length">Lunghezza Password:</label>
+    
+    <input type="number" name="pass-length" id="pass-length" min="4" max="">
+    <button type="submit">Genera</button>
+</form>
+<h3>La tua password generata è:</h3>
+<div>
+        
+    <?php
+        echo generatePassword($passlength, $alphabet);
+    ?>
+</div>
+    </main>
 </body>
 </html>
