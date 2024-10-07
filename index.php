36dce6f582d5ef7dbc3db2c93844a628757ef383
natdm02 <natalidimarino2002@gmail.com>
2024-10-07 16:01:26 +0200

    [add] milestone1-random password function

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
+<?php
+    function passwordCasuale($lunghezza){
+        $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+';
+        $password = '';
+
+        for ($i = 0; $i < $lunghezza; $i++){
+            $password = $caratteri [rand(0,strlen($caratteri)-1)];
+        }
+
+        return $password;
+    }
+
+    if (isset($_GET['lenght'])){
+        $lunghezza = intval($_GET['lenght']);
+
+        echo "<p><strong>Var dump della lunghezza:</strong></p>";
+
+        var_dump($lunghezza);
+
+        if ($lunghezza > 0) {
+
+            $passwordGenerata = generaPasswordCasuale($lunghezza);
+
+            
+
+            var_dump($passwordGenerata);
+
+            
+        }else{
+            $errorMessage = "inserisci un numero maggiore di 0";
+        }
+
+    }
+?>
+    
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +46,13 @@
 </head>
 <body>
     <main>
-        <h1>password</h1>
+       <h1>password casuale:</h1>
+
+       <form action="GET">
+        <label for="lenght">lunghezza password:</label>
+        <input type="number" id="lenght" name="lenght" min="1" max="50" required>
+        <button type="submit">invia</button>
+       </form>
     </main>
 </body>
 </html>
